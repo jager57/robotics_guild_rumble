b6760e913150d7f78cedafb970743db76c5c688a
jager57 <brennglover@gmail.com>
2017-03-21 15:07:11 -0500

    Update rsvp.php

--- rsvp.php
+++ rsvp.php
@@ -1,7 +1,7 @@
 <?php 
 $file = "list.txt"; 
 $name = $_POST['name']+" "; 
-$write = fopen("$file$ex","a");  
+$write = fopen("$file","a");  
 fwrite($write,$name); 
 fclose($write);
 header('Location: https://jager57.github.io/robotics_guild_rumble');
